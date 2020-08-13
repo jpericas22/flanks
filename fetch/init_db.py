2437095f912a91c1cb4f127b4c99b766d7f93a0f
Jaime Pericás <jpericas22@gmail.com>
2020-08-13 17:26:49 +0200

    init_db fix

--- fetch/init_db.py
+++ fetch/init_db.py
@@ -16,25 +16,23 @@ mongo_client = MongoClient(
     'mongodb://'+DB_USER+':'+DB_PASSWORD+'@database:27017')
 db = mongo_client[DB_NAME]
 
+
 def hash_password(password):
     salt = secrets.token_hex(16)
     return hashlib.pbkdf2_hmac('sha256', password.encode(ENCODING), salt.encode(ENCODING), 100000).hex(), salt
 
 
-def run():
-    pubkey = crypto.gen_keys(ADDRESS)
-    (password_hash, salt) = hash_password(USER_PASSWORD)
-    default_user = {
-        'username': 'client',
-        'password': password_hash,
-        'salt': salt,
-        'addressList': [ADDRESS],
-        'publicKey': pubkey
-    }
-    users_collection = db['users']
-    users_collection.create_index('username', unique=True)
-    users_collection.update_one({'username': default_user['username']}, {
-                                '$set': default_user}, upsert=True)
-    sys.stdout.write('DEBUG:inserted default user to db\n')
-
-run()
+pubkey = crypto.gen_keys(ADDRESS)
+(password_hash, salt) = hash_password(USER_PASSWORD)
+default_user = {
+    'username': 'client',
+    'password': password_hash,
+    'salt': salt,
+    'addressList': [ADDRESS],
+    'publicKey': pubkey
+}
+users_collection = db['users']
+users_collection.create_index('username', unique=True)
+users_collection.update_one({'username': default_user['username']}, {
+                            '$set': default_user}, upsert=True)
+sys.stdout.write('DEBUG:inserted default user to db\n')
