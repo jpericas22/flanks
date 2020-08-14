b13f480b64cfa3c621d9748b40a9c165ad5130dd
Jaime Pericás <jpericas22@gmail.com>
2020-08-14 14:45:28 +0200

    monitor random id

--- monitor/main.py
+++ monitor/main.py
@@ -3,15 +3,18 @@ import sys
 import asyncio
 import websockets
 import cotio_commands
+import random
+import string
 from pprint import pprint
 from json import JSONDecodeError
 
-HOSTNAME = 'mainnet-fullnode1.coti.io'#os.environ['MONITOR_HOSTNAME']
-PATH = '/websocket/140/dv52lpw4/websocket'#os.environ['MONITOR_PATH']
-ADDRESS = '51dbd2feecb8c9e3b5c88129da88156d738d00d57bf4524cc780221c4e414ffc9372b00ad7d75679032d928776b044d40d5febb783d8ac9b241b7c0b1cad77de9b699c23'
+HOSTNAME = os.environ['MONITOR_HOSTNAME']
+ADDRESS = os.environ['ADDRESS']
 
 async def monitor():
-    uri = "wss://" + HOSTNAME + PATH
+    random_id = ''.join(random.choice(string.ascii_uppercase + string.digits)
+            for _ in range(8))
+    uri = "wss://" + HOSTNAME + '/websocket/140/' + random_id + '/websocket'
     async with websockets.connect(uri, ssl=True) as websocket:
         await websocket.recv()
         await websocket.send(cotio_commands.CONNECT)
